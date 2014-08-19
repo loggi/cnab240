ab765f44992bec08742fd101fb2f6af48ae59ecb
Henry John Kupty <hkupty@gmail.com>
2014-08-19 16:59:51 -0300

    Test runner: Check version and call correctly

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-python -m unittest discover || unit2 discover
+PY_VER=$(python -c "import sys; print(sys.version.split(' ')[0])")
+
+baseline() {
+    echo -e "$1\n2.7" | sort -V | tail -n1
+}
+
+check() {
+    [ "$1" = $(baseline $1) ]
+}
+
+if check $PY_VER; then
+    python -m unittest discover
+else
+    python -m unit2 discover
+fi
