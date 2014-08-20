3bac7b9e7a0fb4ad16a6b856e435182da7db00cb
Henry John Kupty <hkupty@gmail.com>
2014-08-19 22:41:57 -0300

    Add: Support profiling

---
+++ tests/profile.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+""" Small helper. """
+
+import cProfile
+import pstats
+
+
+def measure(func):
+    """ The decorator. """
+    def profiling(*args, **kwargs):
+        """ The profiler. """
+        prof = cProfile.Profile()
+        prof.enable()
+        ret = prof.runcall(func, *args, **kwargs)
+        prof.disable()
+        ps = pstats.Stats(prof).sort_stats('cumulative')
+        print func.func_name
+        ps.print_stats(20)
+        return ret
+
+    profiling.func_name = func.func_name
+    return profiling
