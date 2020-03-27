eccafb37e18943eaacb361bc7edc026d0cdab925
leonardo.oliveira <leonardo.oliveira@loggi.com>
2020-03-27 19:13:24 -0300

    bump version to 1.2.0.loggi-1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from cnab240 import __version__
 
 setup(
-    name='loggicnab240',
+    name='cnab240',
     version=__version__,
     author='Loggi Tecnologia LTDA',
     author_email='dev@loggi.com',
@@ -15,7 +15,7 @@ setup(
     zip_safe=False,
     install_requires=[],
     provides=[
-        'loggicnab240'
+        'cnab240'
     ],
     license='LGPL',
     description='Classe para gerar arquivo de remessa e leitura de retorno no '
